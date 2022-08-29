f09f98b1890f3627e634f17757751eca5172dd8e
Abhinav <abhinav.sattiraju@gmail.com>
2022-08-29 18:05:49 -0400

    officer and footer done

---
+++ src/components/footer.js
@@ -0,0 +1,16 @@
+
+import slack from '../slack.png'
+
+export const Footer = (props) => {
+    return (
+        <div className="footer">
+            <div className='links'>
+                <div>
+                    Check out our Slack!    
+                </div>
+                <a href='https://aimsatgt.slack.com/' target={'_blank'}><img src={slack} className="slack" alt="logo" /></a>
+            </div>
+            <div className='club-text'>A.I.M.S. at GT, 2022.</div>
+        </div>
+    )
+}
